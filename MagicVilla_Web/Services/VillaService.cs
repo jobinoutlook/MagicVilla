51b61396bd1a2c26a17bdfd14962336905f7ed24
jobin jose samuel <jobin.samuel@outlook.com>
2024-09-05 20:07:58 +0530

    Magic Villa Web

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,47 @@
+﻿using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string _villaUrl;
+
+        public VillaService(IHttpClientFactory clientFactory,IConfiguration configuration ) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            _villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new Models.APIRequest()
+            {
+                ApiType=MagicVilla_Utility.SD.ApiType.POST,
+                Data = dto,
+                Url=_villaUrl
+            })
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
