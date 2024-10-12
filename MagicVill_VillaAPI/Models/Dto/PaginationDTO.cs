784494fbfbd7ae1cd5f13a09d590a5444eda90f9
jobin jose samuel <jobin.samuel@outlook.com>
2024-10-12 11:02:01 +0530

    pagination

---
+++ MagicVill_VillaAPI/Models/Dto/PaginationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class PaginationDTO
+    {
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
